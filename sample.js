3e2f264912f48be29dc27376227dfc8ef0adff2d
faizul <faizulcse@gmail.com>
2023-08-08 16:43:13 +0600

    add users options

--- sample.js
+++ sample.js
@@ -1,11 +1,15 @@
 import http from 'k6/http';
-import {sleep} from 'k6';
+import {check, sleep} from 'k6';
 
+export const options = {
+    vus: 10,
+    duration: "10s"
+}
 export default function () {
     const baseUrl = "https://reqres.in/"
     const endPoint = "api/users/2";
 
     const res = http.get(`${baseUrl}${endPoint}`);
-    console.log(res);
+    check(res, {'is status 200': (r) => r.status === 200});
     sleep(1);
 }
