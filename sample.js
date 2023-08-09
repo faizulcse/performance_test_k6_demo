fd88461fe7b0b4c5b162844cbfb49e860b8f54e0
faizul <faizulcse@gmail.com>
2023-08-09 16:41:02 +0600

    Update sample.js

--- sample.js
+++ sample.js
@@ -1,11 +1,42 @@
-import http from 'k6/http';
 import {check, sleep} from 'k6';
+import exec from "k6/execution";
+import http from "k6/http";
+
+export const users = JSON.parse(open("data.json"))
+
 
 export const options = {
-    vus: 10,
-    duration: "10s"
+    scenarios: {
+        accountCreate: {
+            executor: "per-vu-iterations",
+            vus: 5,
+            iterations: 10
+        },
+        accountCreate2: {
+            executor: "shared-iterations",
+            vus: 3,
+            iterations: 10
+        },
+        accountCreate3: {
+            executor: "constant-vus",
+            vus: 10,
+            duration: '10s'
+        },
+        accountCreate4: {
+            executor: "ramping-vus",
+            startVUs: 0,
+            stages: [
+                {duration: '20s', target: 10},
+                {duration: '30s', target: 10},
+                {duration: '0s', target: 0},
+            ],
+            gracefulRampDown: '30s'
+        }
+
+    }
 }
 export default function () {
+    console.info(`Iterations id ==>  ${exec.scenario.iterationInTest} VU id ==> ${exec.vu.idInTest}`);
     const baseUrl = "https://reqres.in/"
     const endPoint = "api/users/2";
 
